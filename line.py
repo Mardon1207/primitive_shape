bda8ad3811c96b612f3337597f920a862cbeb78e
Mardon1207 <mardonsultonov8@gmail.com>
2023-06-01 22:44:48 +0500

    2

--- line.py
+++ line.py
@@ -1,3 +1,4 @@
+from math import sqrt
 class Line:
     def __init__(self, x1, y1, x2, y2) -> None:
         self.x1 = x1
@@ -14,4 +15,11 @@ class Line:
         Returns:
             float or int: distance.
         """
-        pass
+        x1=self.x1
+        y1=self.y1
+        x2=self.x2
+        y2=self.y2
+        l=sqrt((x2-x1)**2+(y2-y1)**2)
+        return l
+x=Line(3,4,0,0)
+print(x.get_length())
